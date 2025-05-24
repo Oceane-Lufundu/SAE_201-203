c5439c25b37d88ce08d8d8f262634e2096482ecb
jeremy-paradis <paradisjeremy64@gmail.com>
2025-05-24 21:50:28 +0200

    Update confirmation.php

--- BDD/confirmation.php
+++ BDD/confirmation.php
@@ -1,21 +1,20 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <title>Mon Site</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../CSS/style_2.css">
     </head>
     <body>
-        <ul id="nav">
+    <ul id="nav">
             <li class="nav-item-logo">
-                <img src="../image/logo-UGE.jpg" class="logo" height="100px">  
+                <img src="../Images/logo-univ.png" class="logo" height="75px">  
             </li>
             
             <li id="text">
-                <h2>Ajout de nouveau matériel</h2>
+                <h1>Compte admin</h1>
             </li>  
-        </ul>
+    </ul> 
     <h2>Le matériel a été ajouté avec succès ! </h2>
     </body>
